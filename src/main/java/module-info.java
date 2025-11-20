2e35f30e90ced9d149fa5fe7cc681955ba1e8614
Luis Bermejo Lara <luis.b.l.2005@gmail.com>
2025-11-20 12:13:45 +0100

    Estructura de Datos del Proyecto

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,11 +3,13 @@ module luis.gestorfacturasempresa {
     requires javafx.fxml;
     requires java.sql;
 
-    opens luis.gestorfacturasempresa.Controlador to javafx.fxml;     // para el fx:controller
     opens luis.gestorfacturasempresa to javafx.fxml;
     opens Modelo to javafx.fxml;
     opens Controlador to javafx.fxml;
+    opens DAO to javafx.fxml;
+    
     exports luis.gestorfacturasempresa;
     exports Modelo;
     exports Controlador;
+    exports DAO;
 }
