ada304a7a4d250c5f147dea9027965c10cfcc9df
Luis Bermejo Lara <luis.b.l.2005@gmail.com>
2025-11-28 11:05:20 +0100

    Mejora de interfaces y cambio de nombres en controladores y fxml

--- src/main/java/Controlador/NuevaEmpresaController.java
+++ src/main/java/Controlador/NuevaEmpresaController.java
@@ -1,10 +1,14 @@
 package Controlador;
 
+import DAO.EmpresaDAO;
+import Modelo.Empresa;
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
@@ -13,10 +17,25 @@ public class NuevaEmpresaController {
     @FXML
     private ImageView retroceder; // Imagen punta de flecha para volver al menú anterior
 
+    // Campos de Contacto Empresa
+    @FXML
+    private TextField txtEmpresaNombre, txtEmpresaNif, txtEmpresaTelefono, txtEmpresaCorreo, txtEmpresaWeb, txtEmpresaDfiscal, txtEmpresaContacto;
+
+    // Campos de Localización Empresa
+    @FXML
+    private TextField txtEmpresaDireccion, txtEmpresaPais, txtEmpresaCiudad, txtEmpresaProvincia, txtEmpresaCp;
+
+    // botón "Aceptar"
+    @FXML
+    private Button btnAceptar;
+
+    private EmpresaDAO empresaDAO = new EmpresaDAO();
+
     @FXML
     private void initialize() {
         // Añadimos el evento de clic a la flecha de retroceder
         retroceder.setOnMouseClicked(event -> volverAPrincipal());
+        btnAceptar.setOnAction(e -> guardarEmpresa());
     }
 
     @FXML
@@ -44,6 +63,28 @@ public class NuevaEmpresaController {
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+    }
+
+    @FXML
+    private void guardarEmpresa() {
+                Empresa empresa = new Empresa();
+        empresa.setNombre(txtEmpresaNombre.getText().trim());
+        empresa.setNif(txtEmpresaNif.getText().trim());
+        empresa.setTelefono(txtEmpresaTelefono.getText().trim());
+        empresa.setEmail(txtEmpresaCorreo.getText().trim());
+        empresa.setWeb(txtEmpresaWeb.getText().trim());
+        empresa.setDomicilioFiscal(txtEmpresaDfiscal.getText().trim());
+        empresa.setDireccion(txtEmpresaDireccion.getText());
+        empresa.setPais(txtEmpresaPais.getText().trim().trim().isEmpty() ? "España" : txtEmpresaPais.getText().trim());
+        empresa.setCiudad(txtEmpresaCiudad.getText().trim());
+        empresa.setProvincia(txtEmpresaProvincia.getText().trim());
+        empresa.setCp(txtEmpresaCp.getText().trim());
+        empresa.setContacto(txtEmpresaContacto.getText().trim());
+
+        empresaDAO.añadir(empresa);
+
+        // Volver al menú principal
+        volverAPrincipal();
     }
 
 }
